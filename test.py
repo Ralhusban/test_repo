50b5f29230f17307da4b9324ff30862236c807be
Raad Al-Husban <ralhusban@instructors.2u.com>
2024-08-01 20:22:16 -0400

    viz added

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 # This is a test and its now updated
 # This is a change to the python test file
+from matplotlib import pyplot as plt
+import numpy as np
 
-
-
-
-
-import numpy as npy
-
-print(np.random())
+print(np.random.random())
+x= np.arange(0,10,0.1)
+plt.plot(x, np.exp(x))
+plt.title("Exponential line")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.grid()
+plt.show()
